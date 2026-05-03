6ad4558b9c3992bd82ab360a0ff2a852bd59dbeb
Emilson de Souza Nascimento <emilsonsn@users.noreply.github.com>
2026-05-03 13:08:57 -0300

    style: format welcome route function for consistency

--- routes/api.php
+++ routes/api.php
@@ -11,7 +11,7 @@ use App\Http\Controllers\PlanController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function(){
+Route::get('/', function () {
     return response()->json(['message' => 'Welcome to the Memory API']);
 });
 
